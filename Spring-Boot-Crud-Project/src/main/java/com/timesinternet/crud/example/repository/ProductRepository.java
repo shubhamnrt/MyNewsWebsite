5547b7a216274c9d18b40278d75f3b57a7a9d56a
shubhamnrt <shubhamnrt@gmail.com>
2021-08-14 20:08:31 +0530

    created the project

---
+++ Spring-Boot-Crud-Project/src/main/java/com/timesinternet/crud/example/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.timesinternet.crud.example.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.timesinternet.crud.example.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+	
+	Product findByName(String Name);
+
+}
