167d343914f4a4afcb4c66b957668b514fa61601
Rahul Ranjan <rahul.ranjan@timesinternet.in>
2021-08-24 10:20:40 +0530

    security added

--- MyNewsSite/src/main/java/com/til/newswebsite/MyNewsSiteApplication.java
+++ MyNewsSite/src/main/java/com/til/newswebsite/MyNewsSiteApplication.java
@@ -1,32 +1,10 @@
 package com.til.newswebsite;
 
-import com.til.newswebsite.entity.User;
-import com.til.newswebsite.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 @SpringBootApplication
 public class MyNewsSiteApplication {
-
-    @Autowired
-    private UserRepository repository;
-
-    @PostConstruct
-    public void initUsers() {
-        List<User> users = Stream.of(
-                new User(101, "user0", "pwd0", "user0@gmail.com"),
-                new User(102, "user1", "pwd1", "user1@gmail.com"),
-                new User(103, "user2", "pwd2", "user2@gmail.com"),
-                new User(104, "user3", "pwd3", "user3@gmail.com")
-        ).collect(Collectors.toList());
-        repository.saveAll(users);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(MyNewsSiteApplication.class, args);
--- MyNewsSite/src/main/java/com/til/newswebsite/service/impl/UserServiceImpl.java
+++ MyNewsSite/src/main/java/com/til/newswebsite/service/impl/UserServiceImpl.java
@@ -2,16 +2,32 @@ package com.til.newswebsite.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.til.newswebsite.entity.User;
 import com.til.newswebsite.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 @Service
 public class UserServiceImpl {
 
 	@Autowired
 	private UserRepository repository;
+
+
+	@PostConstruct
+	public void initUsers() {
+		List<User> users = Stream.of(
+				new User(101, "user0", "pwd0", "user0@gmail.com"),
+				new User(102, "user1", "pwd1", "user1@gmail.com"),
+				new User(103, "user2", "pwd2", "user2@gmail.com"),
+				new User(104, "user3", "pwd3", "user3@gmail.com")
+		).collect(Collectors.toList());
+		repository.saveAll(users);
+	}
 	
 	public User saveUser(User user) {
 		return repository.save(user);
